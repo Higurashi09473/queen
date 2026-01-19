401efe9ea8490990fcc9e93fe90af88e11f4033b
Kopatich <honeyniljob@gmail.com>
2026-01-19 03:14:35 +0300

    Rename quoteIdentifier to QuoteIdentifier in tests

--- drivers/cockroachdb/cockroachdb_test.go
+++ drivers/cockroachdb/cockroachdb_test.go
@@ -50,7 +50,7 @@ func TestQuoteIdentifier(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result := quoteIdentifier(tt.input)
+			result := QuoteIdentifier(tt.input)
 			if result != tt.expected {
 				t.Errorf("quoteIdentifier(%q) = %q; want %q", tt.input, result, tt.expected)
 			}
